5e36baf63fc6e222d3faeffd43fa0bcb79701d88
romeelk <romeel_k@yahoo.co.uk>
2023-02-01 09:00:01 +0000

    Fixed signature algorithm

--- scripts/generate_client_assertion.py
+++ scripts/generate_client_assertion.py
@@ -15,6 +15,9 @@ from cryptography.x509 import load_pem_x509_certificate
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature import pkcs1_15
 
 def base64_decode_url(data):
     value = data.replace('-', '+').replace('_', '/')
@@ -38,7 +41,7 @@ def get_claims(tenant_id, client_id) -> dict:
         "aud": aud ,
         "exp": valid_to ,
         "iss": confidential_client_id ,
-        "jti": str(uuid.uuid1),
+        "jti": str(uuid.uuid1()),
         "nbf": valid_from ,
         "sub": confidential_client_id 
     }
@@ -47,42 +50,51 @@ def get_claims(tenant_id, client_id) -> dict:
 def get_signed_client_assertion():
 
     absolute_path = os.path.dirname(__file__)
-    print(absolute_path)
     relative_path = "testcertkey.key"
     full_path = os.path.join(absolute_path, relative_path)  
     private_key_pem = Path(full_path).read_text()
     
     rsa_key =  load_pem_private_key(private_key_pem.encode('utf-8'),password=None)
     public_key_path  = os.path.join(absolute_path, "testcert.pem")  
-    print(public_key_path)
+
     x509_certificate = load_pem_x509_certificate(Path(public_key_path).read_text().encode())
   
-    # print thumbprint
-    # print(x509_certificate.fingerprint(algorithm=hashes.SHA1()).hex())
+    x5t = base64.urlsafe_b64encode(x509_certificate.fingerprint(hashes.SHA1())).rstrip(b'=')
     header = {
         "alg": "RS256",
         "typ": "JWT",
-        "x5t": encode_base64_url(x509_certificate.signature)
+        "x5t": x5t.decode()
     }
 
     tenant_id = str("0893480d-4a15-4aa8-b46b-75e6bf0a63f4")
     client_id = str("6467738c-c2b4-4d76-ba04-073f885c72e0")
     claims = get_claims(tenant_id=tenant_id,client_id=client_id)
 
-    header_bytes = json.dumps(header,indent=4, default=str).encode('utf-8')   
-    claims_bytes = json.dumps(claims, indent=4, default=str).encode('utf-8')
-    token =  encode_base64_url(header_bytes) + "." + encode_base64_url(claims_bytes)
+    header_json= json.dumps(header, separators=(',', ':'))
+    header_bytes = header_json.encode('utf-8')
+    claims_json = json.dumps(claims, separators=(',', ':'))
+    claims_bytes = claims_json.encode('utf-8')
 
-    signed_token =  rsa_key.sign(token.encode("UTF-8"), padding.PSS(
-        mgf=padding.MGF1(hashes.SHA256()),
-        salt_length=padding.PSS.MAX_LENGTH
-    ),hashes.SHA256())
+    header_b64 = base64.urlsafe_b64encode(header_bytes).rstrip(b'=').decode('utf-8')
+    claims_b64 = base64.urlsafe_b64encode(claims_bytes).rstrip(b'=').decode('utf-8')
 
-    signed_client_assertion = token + "." + encode_base64_url(signed_token)
+    token =  header_b64 + "." + claims_b64
+   
+    # PADDING WAS WRONG SHOULD BE PKCS1v15 - below does not calculate correct signature
+    # signed_token =  rsa_key.sign(token.encode('UTF-8'),
+    # padding.PSS(
+    #     mgf=padding.MGF1(hashes.SHA256()),
+    #     salt_length=padding.PSS.MAX_LENGTH),
+    # hashes.SHA256())
+    #signature = rsa_key.sign(header_bytes + b'.' + claims_bytes,
+    # WORKS!!!
+    signature = rsa_key.sign(token.encode('utf-8'),
+                             padding.PKCS1v15(), hashes.SHA256())
+    signature_b64 = base64.urlsafe_b64encode(signature).rstrip(b'=').decode('utf-8')
 
-    print(signed_client_assertion)
-    return signed_client_assertion
+    jwt = header_b64 + '.' + claims_b64 + '.' + signature_b64
 
-
+    print(f'JWT token: {str(jwt)}')
+    return jwt
     
-header = get_signed_client_assertion()
+client_assertion = get_signed_client_assertion()
---
+++ src/GenerateClientAssertion/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+   
+        X509Certificate2 cert = new X509Certificate2("/Users/romeel.khan@contino.io/dev/GetBearerFunctionApp/scripts/testcert.pfx", "", X509KeyStorageFlags.Exportable);
+
+        Console.WriteLine("Loaded cert");
+
+        var assertion = GetSignedClientAssertion(cert, "0893480d-4a15-4aa8-b46b-75e6bf0a63f4", "6467738c-c2b4-4d76-ba04-073f885c72e0");
+        Console.WriteLine(assertion);
+        static string Base64UrlEncode(byte[] arg)
+        {
+            char Base64PadCharacter = '=';
+            char Base64Character62 = '+';
+            char Base64Character63 = '/';
+            char Base64UrlCharacter62 = '-';
+            char Base64UrlCharacter63 = '_';
+
+            string s = Convert.ToBase64String(arg);
+            s = s.Split(Base64PadCharacter)[0]; // RemoveAccount any trailing padding
+            s = s.Replace(Base64Character62, Base64UrlCharacter62); // 62nd char of encoding
+            s = s.Replace(Base64Character63, Base64UrlCharacter63); // 63rd char of encoding
+
+            return s;
+        }
+
+        static string GetSignedClientAssertion(X509Certificate2 certificate, string tenantId, string clientId)
+        {
+            // Get the RSA with the private key, used for signing.
+            var rsa = certificate.GetRSAPrivateKey();
+
+            //alg represents the desired signing algorithm, which is SHA-256 in this case
+            //x5t represents the certificate thumbprint base64 url encoded
+            var header = new Dictionary<string, string>()
+            {
+                { "alg", "RS256"},
+                { "typ", "JWT" },
+                { "x5t", Base64UrlEncode(certificate.GetCertHash()) }
+            };
+
+            //Please see the previous code snippet on how to craft claims for the GetClaims() method
+            var claims = GetClaims(tenantId, clientId);
+
+            var headerBytes = JsonSerializer.SerializeToUtf8Bytes(header);
+            var claimsBytes = JsonSerializer.SerializeToUtf8Bytes(claims);
+            string token = Base64UrlEncode(headerBytes) + "." + Base64UrlEncode(claimsBytes);
+
+            string signature = Base64UrlEncode(rsa.SignData(Encoding.UTF8.GetBytes(token), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
+            string signedClientAssertion = string.Concat(token, ".", signature);
+            return signedClientAssertion;
+        }
+        static IDictionary<string, object> GetClaims(string tenantId, string clientId)
+        {
+            //aud = https://login.microsoftonline.com/ + Tenant ID + /v2.0
+            string aud = $"https://login.microsoftonline.com/{tenantId}/oauth2/token";
+
+            string ConfidentialClientID = clientId; //client id 00000000-0000-0000-0000-000000000000
+            const uint JwtToAadLifetimeInSeconds = 60 * 10; // Ten minutes
+            DateTimeOffset validFrom = DateTimeOffset.UtcNow;
+            DateTimeOffset validUntil = validFrom.AddSeconds(JwtToAadLifetimeInSeconds);
+
+            return new Dictionary<string, object>()
+            {
+                { "aud", aud },
+                { "exp", validUntil.ToUnixTimeSeconds() },
+                { "iss", ConfidentialClientID },
+                { "jti", Guid.NewGuid().ToString() },
+                { "nbf", validFrom.ToUnixTimeSeconds() },
+                { "sub", ConfidentialClientID }
+            };
+        }
+    }
+}
